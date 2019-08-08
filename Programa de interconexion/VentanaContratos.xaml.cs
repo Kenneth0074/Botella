b32819efbaef41519aaab53769fd9dd99771833f
Kenneth0074 <53836871+Kenneth0074@users.noreply.github.com>
2019-08-08 16:21:15 -0500

    Add files via upload

---
+++ Programa de interconexion/VentanaContratos.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Programa_de_interconexion
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaContratos.xaml
+    /// </summary>
+    public partial class VentanaContratos : Window
+    {
+        public VentanaContratos()
+        {
+            InitializeComponent();
+        }
+    }
+}
